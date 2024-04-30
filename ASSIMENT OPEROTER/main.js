ed5550bacc70e0d47d79e3a3c25da5b72304c4e7
Shahbaz <167298370+Shahbazabro044@users.noreply.github.com>
2024-04-30 23:32:13 +0500

    Add files via upload

---
+++ ASSIMENT OPEROTER/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let a = 10;
+a += 9;
+console.log(a);
+let b = 34;
+b += 6;
+console.log(b);
+let c = 78;
+c -= 10;
+console.log(c);
---
+++ ASSIMENT OPEROTER/main.ts
@@ -0,0 +1,11 @@
+let a = 10 
+a += 9
+console.log(a)
+
+let b = 34
+b +=6
+console.log(b)
+
+let c = 78
+c -= 10
+console.log(c) 
